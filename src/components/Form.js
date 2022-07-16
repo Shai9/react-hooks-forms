4bd59818ea96d42e1daa760b8b969e731522181f
Dennis Shakava <dennis.shakava@student.moringaschool.com>
2022-07-16 15:53:47 +0300

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,45 @@
+// import React, { useState } from "react";
+
+// function Form() {
+//   const [firstName, setFirstName] = useState("John");
+//   const [lastName, setLastName] = useState("Dennis");
+
+//   return (
+//     <form>
+//       <input type="text" onChange={handleFirstNameChange} value={firstName} />
+//       <input type="text" onchange ={handleLastNameChange} value={lastName} />
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// }
+// function handleFirstNameChange(event) {
+//   setFirstName(event.target.value);
+// }
+
+// function handleLastNameChange(event) {
+//   setLastName(event.target.value);
+// }
+
+// export default Form;
+
 import React, { useState } from "react";
 
 function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+  }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+  }
+
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
